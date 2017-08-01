3363cd73f46b2f2a873fbf02b4cc7f11816eae54
Hui Xie <hui-xie@uiowa.edu>
2017-08-01 18:39:31 -0500

    finish the network construction

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import tensorflow as tf
 import sys
 import numpy as np
 
+InputWidth = 2
+LabelWidth = 26
+
 def usage():
   usageInfo = "Usage:\n"\
          + "command T1T2LabelFilename Epoches HiddenLayerStructure LearningRate [NumberofTrainExample]\n"\
@@ -16,9 +19,6 @@ def usage():
   print(usageInfo)
 
 def main():
-  #hello = tf.constant('Hello, TensorFlow!')
-  #sess = tf.Session()
-  #print(sess.run(hello))
   argc = len(sys.argv)
   if 5 != argc and 6 != argc:
      print("Error: the number of input parameters is incorrect. Quit.")
@@ -26,12 +26,11 @@ def main():
      return;
   filename = sys.argv[1]
   fileData = open(filename)
-  fileBegin = fileData.tell()
   epoches = int(sys.argv[2])
-  hiddrenLayerList = sys.argv[3]
+  hiddrenLayerList = np.asarray(sys.argv[3].split(','))
   learningRate = float(sys.argv[4])
   totalExamples = sum(1 for line in fileData)
-  fileData.seek(fileBegin)
+  fileData.seek(0)
   numTrainTestExamples = 0;
   if 5 == argc:
       numTrainTestExamples = int(totalExamples* 0.8)
@@ -39,17 +38,17 @@ def main():
       numTrainTestExamples = int(sys.argv[5])
 
   # read csv file
-  data  = np.ndarray(shape=(totalExamples,2), dtype=int)
-  label = np.ndarray(shape=(totalExamples,1), dtype=int)
+  totalExamples = 1000 ##################################### for test
+  data  = np.ndarray(shape=(totalExamples,InputWidth), dtype=int)
+  label = np.zeros(shape=(totalExamples,LabelWidth), dtype=int)
   row = 0
   for line in fileData:
     rowList = line.split(',')
     rowList.pop(-1)
-    rowWidth = len(rowList)
-    data[row] = rowList[0:rowWidth-1]
-    label[row] = rowList[rowWidth-1]
+    data[row] = rowList[0:InputWidth]
+    label[row][int(rowList[InputWidth])] = 1
     row += 1
-    if row > 1000: break # for test
+    if row >= 1000: break ##################################### for test
   fileData.close()
 
   #split train and test data and label
@@ -60,11 +59,41 @@ def main():
   testData = data[nTrain:totalExamples]
   testLabel = label[nTrain:totalExamples]
 
-  
+  #Consturct Deep Learning network model
+  x = tf.placeholder(tf.float32,[None,InputWidth])
+  yGroundTruth = tf.placeholder(tf.int16,[None,LabelWidth])
+  preWidth = InputWidth
+  preX = x
+  for width in hiddrenLayerList:
+    W = tf.Variable(tf.zeros([preWidth,width]))
+    b = tf.Variable(tf.zeros([width]))
+    preX = tf.nn.relu(tf.matmul(preX, W) + b)
+    preWidth = width
+  cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=yGroundTruth,logits=preX))
 
+  # train and test preparation
+  train_step = tf.train.GradientDescentOptimizer(learning_rate=learningRate).minimize(cross_entropy)
+
+
+  # train
+  session = tf.Session()
+  session.run(tf.global_variables_initializer())
+  batchSize = 100
+
+  print("Epoch, HiddenLayersWidth, BatchSize, LearningRate, NumTestExamples, CorrectRate")
+  for i in range(epoches):
+     for j in range(0, nTrain, batchSize):
+        batchX = trainData[j:j+batchSize]
+        batchY = trainLabel[j:j+batchSize]
+        session.run(train_step, feed_dict={x:batchX, yGroundTruth:batchY})
+     # test in every epoch
+     correct_prediction = tf.equal(tf.argmax(preX, 1), tf.argmax(yGroundTruth, 1))
+     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+     correctRate = session.run(accuracy, feed_dict={x:testData, yGroundTruth:testLabel})
+     print(i,",",hiddrenLayerList,",",batchSize,",",learningRate,",", nTest,",",correctRate)
+
+  session.close()
   print("I love this game")
-
-
 
 if __name__ == "__main__":
    main()
