7c5d86c8e9e8bfa4f5caaaadb4e457df35281e61
Hui Xie <hui-xie@uiowa.edu>
2017-08-03 11:10:40 -0500

    refine and debug details

--- main.py
+++ main.py
@@ -15,18 +15,18 @@ InputWidth = 2
 LabelWidth = 26  # Groundtruth label is from 0 to 25
 batchSize = 100
 
-visualGraph = True
+#visualGraph = True
 
 def usage():
   usageInfo = "Usage:\n"\
          + "python3 main.py T1T2LabelFilename Epoches HiddenLayerStructure LearningRate\n"\
          + "Notes:\n"\
-         + "1  T1T2LabelFilename is the input csv file with a row representing an example and the last column is a label, which is converted form original nii files;\n"\
+         + "1  T1T2LabelFilename is the input csv file with a row representing an example and the last column is a label, which is a converted form original nii files;\n"\
          + "2. Epoches is an integer larger than zero;\n"\
          + "3. HiddenLayerStructure is number string separated by comma without a space. e.g. 80,60,50,50,26\n"\
          + "4. The width of last layer should be the maximum label value plus 1 for classification goal;\n"\
-         + "5. LearningRate is an initial learningRate, a float number larger than 1e-4;\n"\
-         + "6. Example: python3 ./main.py T1T2Label.csv 20  80,60,40,26 0.002\n"
+         + "5. LearningRate is an initial learningRate, a float number larger than 1e-4, which will decay every 3 epoches;\n"\
+         + "6. Usage Example: python3 ./main.py T1T2Label.csv 20  80,60,40,26 0.002\n"
   print(usageInfo)
 
 def main():
@@ -61,7 +61,7 @@ def main():
     rowList = line.split(',')
     rowList.pop(-1)
     data[row] = rowList[0:InputWidth]
-    label[row][int(rowList[InputWidth])] = 1
+    label[row][int(rowList[InputWidth])] = 1  # label is one-hot row vector
     row += 1
   fileData.close()
 
@@ -79,31 +79,32 @@ def main():
 
   # Consturct a Deep Learning network model
   x = tf.placeholder(tf.float32,[None,InputWidth])
-  yGroundTruth = tf.placeholder(tf.int16,[None,LabelWidth])
+  yGroundTruth = tf.placeholder(tf.float32,[None,LabelWidth])
   preWidth = InputWidth
-  #preX = x
-  # for width in hiddenLayerList:
-  #   W = tf.Variable(tf.zeros([preWidth,width]))
-  #   b = tf.Variable(tf.zeros([width]))
-  #   preX = tf.nn.relu(tf.matmul(preX, W) + b)
-  #   preWidth = width
-  #   #if visualGraph:
-  WList =[]
-  bList = []
-  layerResultlList = [x]
-  sizeHiddenLayers = len(hiddenLayerList)
-  for i in range(sizeHiddenLayers):
-      width = hiddenLayerList[i]
-      WList.append( tf.Variable(tf.zeros([preWidth,width])) )
-      bList.append( tf.Variable(tf.zeros([width])) )
-      layerResultlList.append( tf.nn.relu(tf.matmul(layerResultlList[i], WList[i]) + bList[i]) )
-      preWidth = width
+  preOut = x
+  for width in hiddenLayerList:
+    W = tf.Variable(tf.zeros([preWidth,width]))
+    b = tf.Variable(tf.zeros([width]))
+    preOut = tf.nn.relu(tf.matmul(preOut, W) + b)
+    preWidth = width
 
-  cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=yGroundTruth,logits=layerResultlList[-1]))
+  #####Use another method to construct network
+  # WList =[]
+  # bList = []
+  # layerResultlList = [x]
+  # sizeHiddenLayers = len(hiddenLayerList)
+  # for i in range(sizeHiddenLayers):
+  #     width = hiddenLayerList[i]
+  #     WList.append( tf.Variable(tf.zeros([preWidth,width])) )
+  #     bList.append( tf.Variable(tf.zeros([width])) )
+  #     layerResultlList.append( tf.nn.relu(tf.matmul(layerResultlList[i], WList[i]) + bList[i]) )
+  #     preWidth = width
 
-  # train
-  session = tf.Session()
-  session.run(tf.global_variables_initializer())
+  cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=yGroundTruth,logits=preOut))
+
+  # train and test
+  mySession = tf.Session()
+  mySession.run(tf.global_variables_initializer())
   hiddenLayerListStr = "["+ "-".join(str(e) for e in hiddenLayerList)+"]"
   print("Total train examples: ", nTrain)
   print("===============================================================================")
@@ -111,21 +112,20 @@ def main():
   for i in range(epoches):
      train_step = tf.train.GradientDescentOptimizer(learning_rate=learningRate).minimize(cross_entropy)
      if (0 != i and 0 == i % 3 and learningRate > 1.0e-8):
-       learningRate = learningRate * 0.6
+       learningRate = learningRate * 0.7
 
      for j in range(0, nTrain, batchSize):
         batchX = trainData[j:j+batchSize]
         batchY = trainLabel[j:j+batchSize]
-        session.run(train_step, feed_dict={x:batchX, yGroundTruth:batchY})
+        mySession.run(train_step, feed_dict={x:batchX, yGroundTruth:batchY})
 
      # test in every epoch
-     correct_prediction = tf.equal(tf.argmax(layerResultlList[-1], 1), tf.argmax(yGroundTruth, 1))
+     correct_prediction = tf.equal(tf.argmax(preOut, 1), tf.argmax(yGroundTruth, 1))
      accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), 0)
-     correctRate = session.run(accuracy, feed_dict={x:testData, yGroundTruth:testLabel})
-     #correctRate = accuracy.eval(feed_dict={x: testData, yGroundTruth: testLabel}, session=session)
+     correctRate = mySession.run(accuracy, feed_dict={x:testData, yGroundTruth:testLabel})
      print(i,",",hiddenLayerListStr,",",batchSize,",",learningRate,",", nTest,",",correctRate)
+  mySession.close()
 
-  session.close()
   diffTime = time.perf_counter()-startTime
   print("End Tensorflow Neural Network at:", time.strftime("%Y-%m-%d %H:%M:%S %Z", time.localtime(time.time())))
   print("Computation time for Tensorflow Neural Network: ", diffTime, "seconds.")
