5a3df4db94d6eaba1a716640f49e33fa5b751ca3
Hui Xie <hui-xie@uiowa.edu>
2017-08-09 09:04:41 -0500

    stable for best accuracy 94%

--- main.py
+++ main.py
@@ -13,18 +13,18 @@ import time
 import random
 
 LabelWidth = 26  # Groundtruth label is from 0 to 25
-batchSize = 113  #prime number
+batchSize = 100  #prime number
 
 def usage():
   usageInfo = "Usage:\n"\
          + "python3 main.py T1T2LabelFilename Epoches HiddenLayerStructure LearningRate\n"\
          + "Notes:\n"\
-         + "1  T1T2LabelFilename is the input csv file with a row representing an example and the last column is a label, which is a converted csv from original nii files;\n"\
+         + "1  T1T2LabelFilename is the input csv file with a row representing an example and the last column is a label, which is a converted csv from original 3 nii files;\n"\
          + "2. Epoches is an integer larger than zero;\n"\
-         + "3. HiddenLayerStructure is number string separated by comma without spaces, and prime width of layers is really helpful in training. e.g. 199,167,139,101,71,41,26\n"\
+         + "3. HiddenLayerStructure is number string separated by comma without spaces, e.g. 199,167,139,101,71,41,26\n"\
          + "4. The width of last layer should be the maximum label value plus 1 for classification purpose;\n"\
          + "5. LearningRate is an initial learningRate, a float number larger than 1e-4, which will decay every 3 epoches;\n"\
-         + "6. Usage Example: python3 ./main.py T1T2LabelCubic.csv 8 240,200,160,120,80,40,26 0.002\n"
+         + "6. Usage Example: python3 ./main.py T1T2LabelCubic.csv 10 240,200,160,120,80,40,26 0.002\n"
   print(usageInfo)
 
 def main():
@@ -47,12 +47,12 @@ def main():
   hiddenLayerList = [int(elem) for elem in sys.argv[3].split(',')]
   learningRate = float(sys.argv[4])
 
-  # get inputWidth of file
+  # get InputWidth from  input file, that is nFeatures of example
   firstLine = fileData.readline()
   firstLine = firstLine.split(',')
   firstLine.pop(-1)
   InputWidth = len(firstLine) -1 # the last column is a label column
-  # get the number of the total obserbations (examples)
+  # get the number of the total observations (examples)
   fileData.seek(0)
   totalExamples = sum(1 for line in fileData)
   fileData.seek(0)
@@ -77,7 +77,7 @@ def main():
   trainLabel = label[0:nTrain]
   testData = data[nTrain:totalExamples]
   testLabel = label[nTrain:totalExamples]
-  print("Number of features in train data:", data.shape[1])
+  print("Number of features in example(observation) data:", data.shape[1])
   print("Number of train examples: ", nTrain)
   print("Number of test examples: ", nTest)
 
@@ -86,15 +86,15 @@ def main():
   #for i in range(nTest):
   #  if testLabel[i,0] == 1:
   #      nZeroAtFirstCol +=1
-  #print("Rate of 1s at first column of label,which means network output all zeros will get this result): ", nZeroAtFirstCol/nTest)
-  # 0.5247328947864316
+  #print("Rate of 1s at first column of label,which means network output all zeros, all nan, or always maximum at first column, will get this result): ", nZeroAtFirstCol/nTest)
+  # 0.5247328947864316 for cubic case, 0.53711 for 2 pixel case.
   #===============Debug==============================
 
   # start time computation
   print("Start Tensorflow Neural Network at:", time.strftime("%Y-%m-%d %H:%M:%S %Z", time.localtime(time.time())))
   startTime = time.perf_counter()
 
-  # Consturct a Deep Learning network model
+  # Construct a Deep Learning network model
   x = tf.placeholder(tf.float32,[None,InputWidth])
   y_ = tf.placeholder(tf.float32,[None,LabelWidth])  # groundtruth
   preWidth = InputWidth
@@ -102,7 +102,7 @@ def main():
   nLayer = 0
   for width in hiddenLayerList:
     if 2 == InputWidth:
-       # Xavier initialization results all nan output in 56 input pixels case
+       # Xavier initialization results all nan output in 54 input pixels case
        W_name = "W"+str(nLayer)
        W = tf.get_variable(W_name, shape=[preWidth,width], initializer=tf.contrib.layers.xavier_initializer())
     else:
@@ -114,7 +114,7 @@ def main():
        tf.set_random_seed(W_graph_seed*width*preWidth)
        W = tf.Variable(tf.truncated_normal([preWidth, width], mean=W_mean, stddev=W_stddev, seed=W_seed))
 
-    b = tf.Variable(tf.random_uniform([width],minval=0.01,maxval=0.03))
+    b = tf.Variable(tf.random_uniform([width],minval=0.002,maxval=0.002))
     preOut = tf.nn.relu(tf.matmul(preOut, W) + b)
     preWidth = width
     nLayer += 1
@@ -128,6 +128,8 @@ def main():
   print("===============================================================================")
   print("Epoch, LayersWidth, BatchSize, LearningRate, NumTestExamples, CorrectRate")
   for i in range(epoches):
+     if (0 != i and 0 == i % 3 and learningRate > 1.0e-8):
+       learningRate = learningRate * 0.6
      train_step = tf.train.GradientDescentOptimizer(learning_rate=learningRate).minimize(cross_entropy)
      for j in range(0, nTrain, batchSize):
         batchX = trainData[j:j+batchSize]
@@ -138,12 +140,12 @@ def main():
      accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), 0)
      correctRate = mySession.run(accuracy, feed_dict={x:testData, y_:testLabel})
      print(i,",",layerListStr,",",batchSize,",",learningRate,",", nTest,",",correctRate)
-     if (0 != i and 0 == i % 3 and learningRate > 1.0e-8):
-       learningRate = learningRate * 0.6
 
+     #============Debug =======================
      #testOut = mySession.run(preOut, feed_dict={x:testData, y_:testLabel})
      #print("shape of preOut:", testOut.shape)
      #print(testOut)
+     #==============Debug=======================
 
   mySession.close()
 
