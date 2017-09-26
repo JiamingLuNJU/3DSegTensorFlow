cc710221a8c1e69c7fac8e3044f67436d247b2cb
Hui Xie <hui-xie@uiowa.edu>
2017-09-26 13:53:48 -0500

    use sole 56 cores for test

--- SegBrain.sh
+++ SegBrain.sh
@@ -5,7 +5,7 @@ do
   if (($i == 1 || $i %4 ==0))
   then
      echo $i
-     qsub -b y -cwd -N SegBrain_$i -q HJ,COE,UI -pe smp $i  -e ~/temp/segError.txt -o ~/temp/segStdOutput_$i.txt \
+     qsub -b y -cwd -N SegBrain_$i -q HJ,COE,UI -pe smp 56  -e ~/temp/segError.txt -o ~/temp/segStdOutput_$i.txt \
      ~/intel/intelpython3/bin/python3 ~/Projects/3DSegTensorFlow/MultiCoreTest.py ~/temp/T1T2LabelCubicNormalize.csv 20 240,200,160,120,80,40,26 0.002
   fi
 done
