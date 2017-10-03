e70c8c604852b7257ee4f6a3aabffdce57ace9a2
Hui Xie <hui-xie@uiowa.edu>
2017-10-03 14:40:54 -0500

    add nCore parameters

--- SegBrainHPC.sh
+++ SegBrainHPC.sh
@@ -6,6 +6,6 @@ do
   then
      echo $i
      qsub -b y -cwd -N SegBrain_$i -q HJ,COE,UI -pe smp 56  -e ~/temp/segError.txt -o ~/temp/segStdOutput_$i.txt \
-     ~/intel/intelpython3/bin/python3 ~/Projects/3DSegTensorFlow/MultiCoreTest.py ~/temp/T1T2LabelCubicNormalize.csv 20 240,200,160,120,80,40,26 0.002
+     ~/intel/intelpython3/bin/python3 ~/Projects/3DSegTensorFlow/MultiCoreTest.py ~/temp/T1T2LabelCubicNormalize.csv 20 240,200,160,120,80,40,26 0.002 $i
   fi
 done
